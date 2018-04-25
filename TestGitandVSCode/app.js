1ac76929fe73bda73b4f99dcd459ba37b576c59f
RootPaul <38369475+RootPaul@users.noreply.github.com>
2018-04-25 14:30:46 +0200

    initial commit

---
+++ TestGitandVSCode/app.js
@@ -0,0 +1,26 @@
+// init storage
+const storage = new storage()
+// get stored location data
+const weatherLocation = storage.getLocationData();
+// init weather object
+const weather = new weather(weatherLocation.city, weatherLocation.state);
+// init UI
+const ui = new UI();
+
+// get weather on DOM load
+document.addEventListener(DOMContentLoaded,getWeather);
+
+
+{
+  {
+    [
+      {
+        {
+          {
+            
+          }
+        }
+      }
+    ]
+  }
+}
